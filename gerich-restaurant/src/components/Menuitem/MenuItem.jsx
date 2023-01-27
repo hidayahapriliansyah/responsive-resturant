20d1fe80f320ac83a6f74054d7a2993e19266c00
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-27 10:57:45 +0700

    specialmenu section done!

--- gerich-restaurant/src/components/Menuitem/MenuItem.jsx
+++ gerich-restaurant/src/components/Menuitem/MenuItem.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuItem">
+    <div className="app__menuItem-head">
+      <div className="app__menuItem-head_name">
+        <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      </div>
+
+      <div className="app__menuItem-head_dash" />
+
+      <div className="app__menuItem-head_price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuItem-sub">
+      <p className="p__opensans" style={{ color: '#AAA' }}>{tags}</p>
+    </div>
   </div>
+
 );
 
 export default MenuItem;
